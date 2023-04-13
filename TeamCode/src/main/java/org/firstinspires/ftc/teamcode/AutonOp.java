0e6448239dd6962ffb228bc20f11b00410ebdb98
Elizabeth.Do754 <Elizabeth.Do754@stu.kipptexas.org>
2023-04-12 19:13:48 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOp.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "autonMode")
+
+public class AutonOp {
+    Servo claw;
+    DcMotor RightBack;
+    DcMotor
+}

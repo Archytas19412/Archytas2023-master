bb51a9cd9418e75bb72cec544e36a99dfbec743e
Elizabeth.Do754 <Elizabeth.Do754@stu.kipptexas.org>
2023-05-11 13:54:28 -0500

    Initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueRightAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueRightAuton.java
@@ -24,7 +24,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.apriltag.AprilTagDetection;
@@ -32,12 +31,10 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-import org.openftc.easyopencv.OpenCvInternalCamera;
-
 import java.util.ArrayList;
 
 @Autonomous
-public class cameraAuton extends LinearOpMode
+public class blueRightAuton extends LinearOpMode
 {
     OpenCvCamera camera;
     DcMotor frontRightMotor;

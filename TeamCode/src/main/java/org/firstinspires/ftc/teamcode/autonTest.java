4a866c65dcaaf03058196f811708835c109474c9
Elizabeth.Do754 <Elizabeth.Do754@stu.kipptexas.org>
2023-05-13 10:55:14 -0500

    Initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonTest.java
@@ -245,7 +245,7 @@ public class autonTest extends LinearOpMode
             frontRightMotor.setPower(0.5);
             backLeftMotor.setPower(0.5);
             backRightMotor.setPower(-0.5);
-            sleep(1250);
+            sleep(1300);
             frontLeftMotor.setPower(0);
             frontRightMotor.setPower(0);
             backLeftMotor.setPower(0);

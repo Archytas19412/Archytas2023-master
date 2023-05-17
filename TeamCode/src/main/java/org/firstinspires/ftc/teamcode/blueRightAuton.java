6108888d1b5de695d0d0515f0c23524b06f83acb
Elizabeth.Do754 <Elizabeth.Do754@stu.kipptexas.org>
2023-05-17 18:45:08 -0500

    Initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueRightAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueRightAuton.java
@@ -234,7 +234,7 @@ public class blueRightAuton extends LinearOpMode
             frontRightMotor.setPower(-0.5);
             backLeftMotor.setPower(-0.5);
             backRightMotor.setPower(-0.5);
-            sleep(747);
+            sleep(735);
             frontLeftMotor.setPower(0);
             frontRightMotor.setPower(0);
             backLeftMotor.setPower(0);
